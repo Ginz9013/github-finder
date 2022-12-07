25a2776a56e01db48a70180ae215b7f114aac02b
Kouhei Ho <alan829221@gmail.com>
2022-12-07 22:54:27 +0800

    v1.0

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,7 @@
+import spinner from './assets/spinner.gif'
+
+function Spinner() {
+  return <img width={180} className="text-center mx-auto" src={spinner} alt="Loading..." />
+}
+
+export default Spinner
